8c16e7429f9ee5515662660df5766271c902fb81
Kayane Rocha <kayannerch@gmail.com>
2024-07-04 17:56:51 -0300

    refactor: ajustando espaçamento

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
-from blueprints.task import task_blueprint
-from blueprints.user import user_blueprint
 from decouple import config
 
+from blueprints.task import task_blueprint
+from blueprints.user import user_blueprint
+
 app = Flask(__name__)
+
 app.register_blueprint(task_blueprint)
 app.register_blueprint(user_blueprint)
-app.config['SECRET_KEY'] = config('SECRET_KEY')
 
+app.config['SECRET_KEY'] = config('SECRET_KEY')
 
 if __name__ == '__main__':
     app.run(debug=True)

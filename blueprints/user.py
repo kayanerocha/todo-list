f9af0ed55e0f87baf13be5883f8ccba9f4bd0c5b
Kayane Rocha <kayannerch@gmail.com>
2024-07-05 17:50:50 -0300

    feat: confirmação de senha e redirecionando para login

--- blueprints/user.py
+++ blueprints/user.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request, url_for, flash, redirect
 from werkzeug.security import generate_password_hash
 
 from database.database import get_connection
-from blueprints.task import task_blueprint
 
 user_blueprint = Blueprint('user', __name__)
 
@@ -21,18 +20,26 @@ def register():
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
+        confirm_password = request.form['confirm_password']
 
         if len(password) <= 5:
             flash('The password must be at least 6 characters long!')
+        elif password != confirm_password:
+            flash('Passwords must be the same!')
         elif user_exist(email):
-            flash('User already exists!') 
+            flash('User already exists!')
         else:
             hash_password = generate_password_hash(password)
 
             conn = get_connection()
             conn.execute('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', (name, email, hash_password))
             conn.commit()
-            conn.close()       
+            conn.close()
 
-            return redirect(url_for('task.index'))
+            flash('Registration completed successfully!')
+            return redirect(url_for('user.login'))
     return render_template('user/register.html')
+
+@user_blueprint.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('user/login.html')

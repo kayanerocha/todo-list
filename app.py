7c8c7454fb9ccced91ebf98c45cf7471d53b018b
Kayane Rocha <kayannerch@gmail.com>
2024-07-05 17:54:16 -0300

    fix: ajuste das configs do app

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from blueprints.user import user_blueprint
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = config('SECRET_KEY')
+
 app.register_blueprint(task_blueprint)
 app.register_blueprint(user_blueprint)
-
-app.config['SECRET_KEY'] = config('SECRET_KEY')
 
 if __name__ == '__main__':
     app.run(debug=True)

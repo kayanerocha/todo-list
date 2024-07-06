5daa4228086d7add050084dd6c079ccc59b501b8
Kayane Rocha <kayannerch@gmail.com>
2024-07-06 20:17:10 -0300

    feat: configuração do sqlalchemy e db sqlite

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from decouple import config
+from flask_sqlalchemy import SQLAlchemy
 
+import blueprints
 from blueprints.task import task_blueprint
 from blueprints.user import user_blueprint
 
 app = Flask(__name__)
+db = SQLAlchemy()
 
 app.config['SECRET_KEY'] = config('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+db.init_app(app)
+# blueprints.login_manager.init_app(app)
 
 app.register_blueprint(task_blueprint)
 app.register_blueprint(user_blueprint)

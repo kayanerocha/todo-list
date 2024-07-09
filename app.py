7aa771c67f4ca12b805710e413f06ed001c74061
Kayane Rocha <kayannerch@gmail.com>
2024-07-08 21:07:37 -0300

    feat: criando o db com flask alchemy e a função de login

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
 from flask import Flask
 from decouple import config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
-import blueprints
 from blueprints.task import task_blueprint
 from blueprints.user import user_blueprint
+import blueprints.user
 
 app = Flask(__name__)
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 app.config['SECRET_KEY'] = config('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
 db.init_app(app)
-# blueprints.login_manager.init_app(app)
+login_manager.login_view = 'user.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    blueprints.user.get_user(id=user_id)
 
 app.register_blueprint(task_blueprint)
 app.register_blueprint(user_blueprint)
+
+app.app_context().push()
+db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- blueprints/user.py
+++ blueprints/user.py
@@ -1,20 +1,30 @@
 from flask import Blueprint, render_template, request, url_for, flash, redirect
-from werkzeug.security import generate_password_hash
-# from flask_login import LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user
 
 from database.database import get_connection
 import blueprints
 
 user_blueprint = Blueprint('user', __name__)
 
-def user_exist(email):
+# def user_exist(email):
+#     conn = get_connection()
+#     user = conn.execute('SELECT * FROM users where email = ?', (email,)).fetchone()
+#     conn.close()
+
+#     if user:
+#         return user
+#     return False
+
+def get_user(id=None, email=None):
     conn = get_connection()
-    user = conn.execute('SELECT * FROM users where email = ?', (email,)).fetchone()
+    if id:
+        user = conn.execute('SELECT * FROM users where id = ?', (id,)).fetchone()
+    else:
+        user = conn.execute('SELECT * FROM users where email = ?', (email,)).fetchone()
     conn.close()
 
-    if user:
-        return True
-    return False
+    return user
 
 @user_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
@@ -28,7 +38,7 @@ def register():
             flash('The password must be at least 6 characters long!')
         elif password != confirm_password:
             flash('Passwords must be the same!')
-        elif user_exist(email):
+        elif get_user(email=email):
             flash('User already exists!')
         else:
             hash_password = generate_password_hash(password)
@@ -44,6 +54,16 @@ def register():
 
 @user_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
-    # if request.method == 'POST':
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        remember = True if request.form.get('remember') else False
+
+        user = get_user(email=email)
+
+        if user and check_password_hash(user['password'], password):
+            login_user(user=user, remember=remember)
+            return redirect(url_for('task.index'))
+        flash('Please check your login details and try again.')
 
     return render_template('user/login.html')

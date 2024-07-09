1035b8b9806b50a751997c7d0532cc1a5fd3e66c
Kayane Rocha <kayannerch@gmail.com>
2024-07-09 17:48:05 -0300

    fix: load_user para pegar o user logado e drop_all para restaurar o bd

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ from flask import Flask
 from decouple import config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from sys import argv
 
+from database.database import db
 from blueprints.task import task_blueprint
 from blueprints.user import user_blueprint
-import blueprints.user
+from models.user import User
 
 app = Flask(__name__)
-db = SQLAlchemy()
+
 login_manager = LoginManager()
 
 app.config['SECRET_KEY'] = config('SECRET_KEY')
@@ -20,13 +22,15 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    blueprints.user.get_user(id=user_id)
+    return User.query.get(int(user_id))
 
 app.register_blueprint(task_blueprint)
 app.register_blueprint(user_blueprint)
 
-app.app_context().push()
-db.create_all()
+with app.app_context():
+    if 'restore_db' in argv:
+        db.drop_all()
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)

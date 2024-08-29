c79c0e6f66bd0e6d4de7530fcd7ac49bc7240a8a
kayanerocha <kayannerch@gmail.com>
2024-08-29 14:27:18 -0300

    refactor: init e table name

--- models/task.py
+++ models/task.py
@@ -4,6 +4,7 @@ import datetime
 from database.database import db
 
 class Task(db.Model):
+    __tablename__ = 'task'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500))
@@ -12,4 +13,12 @@ class Task(db.Model):
     concluded = db.Column(db.Boolean)
     created = db.Column(TIMESTAMP, default=datetime.datetime.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+    def __init__(self, title, description, start_date, end_date, concluded, user_id) -> None:
+        self.title = title
+        self.description = description
+        self.start_date = start_date
+        self.end_date = end_date
+        self.concluded = concluded
+        self.user_id = user_id
         
--- models/user.py
+++ models/user.py
@@ -5,9 +5,15 @@ import datetime
 from database.database import db
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     created = db.Column(TIMESTAMP, default=datetime.datetime.now())
     tasks = db.relationship('Task', backref='user')
+    
+    def __init__(self, name, email, password) -> None:
+        self.name = name
+        self.email = email
+        self.password = password
